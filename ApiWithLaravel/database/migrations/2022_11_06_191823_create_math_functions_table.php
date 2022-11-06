3f7635f3d975972b36dbf9486eb148a37a808bfa
kmfajardo <kfajardop2@miumg.edu.gt>
2022-11-06 11:28:22 -0800

    new function controller, model, migration added

---
+++ ApiWithLaravel/database/migrations/2022_11_06_191823_create_math_functions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('math_functions', function (Blueprint $table) {
+            $table->id();
+            $table->double('vInicial', 8, 3);
+            $table->double('vFinal', 8, 3);
+            $table->double('vAcel', 8, 3);
+            $table->double('tiempo', 8, 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('math_functions');
+    }
+};
